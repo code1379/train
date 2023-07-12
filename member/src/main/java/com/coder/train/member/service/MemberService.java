055dfd5a78003760dcedb3a63c9cc490a7837226
guojingzhou <guojingzhou@huayun.com>
2023-07-12 17:22:33 +0800

    4-15 实现一个简单的注册接口

--- member/src/main/java/com/coder/train/member/service/MemberService.java
+++ member/src/main/java/com/coder/train/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package com.coder.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.coder.train.member.domain.Member;
+import com.coder.train.member.domain.MemberExample;
 import com.coder.train.member.mapper.MemberMapper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -12,5 +17,23 @@ public class MemberService {
 
     public int count() {
         return (int) memberMapper.countByExample(null);
+    }
+
+    public long register(String mobile) {
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> list = memberMapper.selectByExample(memberExample);
+
+        // 数据中已经存在该手机号，直接返回 id。或者抛出一个用户已经注册了的异常
+        if(CollUtil.isNotEmpty(list)){
+            // return list.get(0).getId();
+            throw new RuntimeException("手机号已经注册了");
+        }
+
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
     }
 }
