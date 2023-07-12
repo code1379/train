ac494babd44b305021a654a44cb6ae2de0444cf6
guojingzhou <guojingzhou@huayun.com>
2023-07-12 16:43:23 +0800

    4-14 集成Mybatis官方代码生成器，访问数据库成功

--- member/src/main/java/com/coder/train/member/mapper/MemberMapper.java
+++ member/src/main/java/com/coder/train/member/mapper/MemberMapper.java
@@ -1,5 +1,31 @@
 package com.coder.train.member.mapper;
 
+import com.coder.train.member.domain.Member;
+import com.coder.train.member.domain.MemberExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface MemberMapper {
-    int count();
+    long countByExample(MemberExample example);
+
+    int deleteByExample(MemberExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Member row);
+
+    int insertSelective(Member row);
+
+    List<Member> selectByExample(MemberExample example);
+
+    Member selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("row") Member row, @Param("example") MemberExample example);
+
+    int updateByExample(@Param("row") Member row, @Param("example") MemberExample example);
+
+    int updateByPrimaryKeySelective(Member row);
+
+    int updateByPrimaryKey(Member row);
 }
